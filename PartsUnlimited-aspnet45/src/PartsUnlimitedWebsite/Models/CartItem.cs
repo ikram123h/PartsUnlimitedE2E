8286c75882f746f968a1ef7d17390aa630078063
Affout Ikram <Ikrame.Affout@emsi-edu.ma>
2023-04-11 10:35:13 +0000

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My second change
+        //My second change.
         [Key]
         public int CartItemId { get; set; }
 
